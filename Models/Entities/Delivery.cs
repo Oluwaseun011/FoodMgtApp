5e32b743b4b227a98502921ae09b1f41cc2f3381
unknown <aimanahmadadeoye1@gmail.com>
2025-12-05 10:32:56 +0100

    Delivery guy arrived

--- Models/Entities/Delivery.cs
+++ Models/Entities/Delivery.cs
@@ -7,6 +7,23 @@ namespace FoodDeliveryApp.Models.Entities
 {
     public class Delivery
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Email {get; set; } = null!;
+        public string PlateNumber { get; set; } = null!;
+        public bool IsAvailable { get; set; }
+        public string CreatedBy { get; set; } = default!;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+
+        public Delivery(string name,  string phoneNumber, string email, string plateNumber, bool isAvailable)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            PlateNumber = plateNumber;
+            IsAvailable = isAvailable;
+        }
     }
 }
