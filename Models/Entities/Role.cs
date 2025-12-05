f0b1763220748f933fc4a49e42b9b72629cebd27
Haseenah-Sho <haseenahshoboyede@gmail.com>
2025-12-05 10:21:24 -0800

    joiner

--- Models/Entities/Role.cs
+++ Models/Entities/Role.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodMgtApp.Models.Entities;
 
 namespace FoodDeliveryApp.Models.Entities
 {
     public class Role
     {
-        
+        public int Id {get; set;}
+        public string Name {get; set;} = default!;
+        public List<UserRole> UserRoles {get; set;} = new List<UserRole>();
+
+
+        public Role(string name)
+        {
+            Name = name;
+        }
     }
 }
---
+++ Models/Entities/UserRole.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDeliveryApp.Models.Entities;
+
+namespace FoodMgtApp.Models.Entities
+{
+    public class UserRole
+    {
+        public int Id{get; set;}
+        public int UserId{get; set;}
+        public int RoleId{get; set;}
+        public User User{get; set;} = default!;
+        public Role Role{get; set;} = default!;
+    }
+}
