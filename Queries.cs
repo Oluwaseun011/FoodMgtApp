1a9114d02f5e81eed6452a0e2d960300bebd9808
unknown <kawtharkehinde@gmail.com>
2025-12-05 12:57:25 +0100

    KitchenManagerTable

--- Queries.cs
+++ Queries.cs
@@ -7,6 +7,7 @@ namespace FoodDeliveryApp
 {
     public class Queries
     {
-        
+        public string KitchenTable = "create table if not exist (Kitchen, id auto_increament, Name varchar (60) not null, Description varchar (200), CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint)";
+        public string ManagerTable = "create table if not exist (Manager, id auto_increament, Email varchar (100) not null, Name varchar (60) not null, KitchenId int, CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint)";
     }
 }
