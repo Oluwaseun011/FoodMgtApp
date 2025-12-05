1aeda1e5bb7346a6abfd760096dca1f8d1f41926
unknown <abdulqauyumayomiposi652@gmail.com>
2025-12-05 10:32:51 +0100

    Initial

--- Models/Entities/Food.cs
+++ Models/Entities/Food.cs
@@ -7,6 +7,21 @@ namespace FoodDeliveryApp.Models.Entities
 {
     public class Food
     {
-        
+        public int Id { get; set;}
+        public string Name{get;set;} = null!;
+        public string Description{get;set;} = null!;
+        public decimal Price{get; set;} 
+        public int CategoryId{get;set;}
+        public int Quantity{get;set;}
+
+        public Food(string name, string description,decimal price,int categoryId,int quantity)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            CategoryId = categoryId;
+            Quantity = quantity;
+        }
+
     }
 }
