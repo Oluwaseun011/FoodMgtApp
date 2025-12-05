b3cfe470d01c68c268829a85bf619c64a0ad8af7
oyekanmi praise <oyekanmipraise07@gmail.com>
2025-12-05 12:24:25 +0100

    corrected code for SecondFourDto

--- Dtos/FoodDto.cs
+++ Dtos/FoodDto.cs
@@ -4,11 +4,12 @@ namespace FoodMgtApp.Dtos
     {
          
         public int Id { get; set;}
+        public int Quantity{get;set;}
         public string Name{get;set;} = default!;
         public string Description{get;set;} = default!;
         public decimal Price{get; set;} 
         public int CategoryId{get;set;}
-        public int Quantity{get;set;}
+        
 
       
     }
