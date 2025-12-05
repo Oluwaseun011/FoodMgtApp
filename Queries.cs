1694e5410cf032ac33ee3f8dd8e88bd084c5fa8e
unknown <kawtharkehinde@gmail.com>
2025-12-05 13:22:14 +0100

    Correction

--- Queries.cs
+++ Queries.cs
@@ -7,7 +7,7 @@ namespace FoodDeliveryApp
 {
     public class Queries
     {
-        public string KitchenTable = "create table if not exist Kitchens (Id auto_increament, Name varchar (60) unique not null, Description varchar (200), CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint, primary key(Id))";
-        public string ManagerTable = "create table if not exist Managers (Id auto_increament, Email varchar (100) unique not null, Name varchar (60) not null, KitchenId int, CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint, primary key(Id), foreign key(KitchenId) references Kitchens (id) )";
+        public string KitchenTable = "create table if not exist Kitchens (Id int auto_increament, Name varchar (60) unique not null, Description varchar (200), CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint, primary key(Id))";
+        public string ManagerTable = "create table if not exist Managers (Id int auto_increament, Email varchar (100) unique not null, Name varchar (60) not null, KitchenId int, CreatedBy varchar (60), DateCreated datetime, IsDeleted tinyint, primary key(Id), foreign key(KitchenId) references Kitchens (id) )";
     }
 }
