37e089e4968a77f54a1df9b00d0cc3ae93a8c895
Haseenah-Sho <haseenahshoboyede@gmail.com>
2025-12-05 11:35:51 -0800

    UserRepo

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodDeliveryApp.Models.Entities;
 
 namespace FoodDeliveryApp.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        void AddToDb(User user);
+        bool IsExist(string email);
+        ICollection<User> GetUsers();
+        User? GetUser(string email);
         
     }
 }
