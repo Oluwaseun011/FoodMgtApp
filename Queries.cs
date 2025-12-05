e050031df92ccaa9f2c1b7b41714ad30699bde36
oyekanmi praise <oyekanmipraise07@gmail.com>
2025-12-05 13:22:55 +0100

    new changes

--- Queries.cs
+++ Queries.cs
@@ -7,7 +7,7 @@ namespace FoodDeliveryApp
 {
     public class Queries
     {
-        public const string CategoryTables = "create table if not exist category(Id int auto_increment,Name varchar(60));";
-        public const string FoodTables = "create table if not exist food(Id int auto_increment,Name varchar(60)) not null,Discription varchar(100),Price decimal  not null,Quantity int, primary key(id), foreign key(CategoryId) references category(Id)); ";
+        public const string CategoryTables = "create table if not exist categories(Id int auto_increment,Name varchar(60),primary key(id));";
+        public const string FoodTables = "create table if not exist foods(Id int auto_increment,Name varchar(60)) not null,Discription varchar(100),Price decimal  not null,Quantity int, primary key(id), foreign key(CategoryId) references category(Id)); ";
     }
 }
