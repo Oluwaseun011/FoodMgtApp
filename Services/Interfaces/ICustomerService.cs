27f5ab1f5c920ade65de96ca16698f29cb502b20
Ajibike Abdulsamad <ajibikeabdulsamad@gmail.com>
2025-12-05 13:07:31 +0100

    I'm done

--- Services/Interfaces/ICustomerService.cs
+++ Services/Interfaces/ICustomerService.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FoodDeliveryApp.Dtos;
+using FoodMgtApp.Dtos;
 
 namespace FoodMgtApp.Services.Interfaces
 {
     public interface ICustomerService
     {
-        LoginResponse Register(LoginRequest request);
-        
+        CustomerResponse Register(CustomerRequest request);
     }
 }
