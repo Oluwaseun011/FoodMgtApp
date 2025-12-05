948cf489c740ba2e0d5f2b4b8b26b3c4f3fe4980
sultaan0x <sultaanjamiuazeez@gmail.com>
2025-12-05 10:26:06 +0100

    customer & wallet

--- Models/Entities/Customer.cs
+++ Models/Entities/Customer.cs
@@ -7,6 +7,18 @@ namespace FoodDeliveryApp.Models.Entities
 {
     public class Customer
     {
+        public int Id {get; set;}
+        public string Name {get; set;} = default!;
+        public string Email {get; set;} = default!;
+        public string Address {get; set;} = default!;
+
+        public Customer(string name, string email,string address)
+        {
+            Name = name;
+            Email = email;
+            Address = address;
+            
+        }
         
     }
 }
--- Models/Entities/Wallet.cs
+++ Models/Entities/Wallet.cs
@@ -7,6 +7,15 @@ namespace FoodDeliveryApp.Models.Entities
 {
     public class Wallet
     {
-        
+        public int Id {get; set;}
+        public double Amount {get; set;} = 0;
+        public int CustomerId {get; set;}
+
+        public Wallet(double amount, int customerId)
+        {
+            Amount = amount;
+            CustomerId = customerId;
+
+        }
     }
 }
