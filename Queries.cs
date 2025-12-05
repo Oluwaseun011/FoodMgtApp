8393ce2d37716528598444aa59555a25aa2f829d
unknown <aimanahmadadeoye1@gmail.com>
2025-12-05 13:06:54 +0100

    Alhamdulillah

--- Queries.cs
+++ Queries.cs
@@ -7,6 +7,7 @@ namespace FoodDeliveryApp
 {
     public class Queries
     {
-        
-    }
+        public const string foodmgtapp = "create schema if not exists foodmgtapp";
+        public const string userrole = "create table if not exists userroles(Id int auto_increment, UserId int, RoleId int, primary key(Id), foreign key(UserId) references user(Id), foreign key(RoleId) references role(Id) );";
+        }
 }
