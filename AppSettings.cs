772d97f7d6917abec6f2b13c914eb33eeefaa0b2
Haseenah-Sho <haseenahshoboyede@gmail.com>
2025-12-05 10:43:04 -0800

    appsettings

---
+++ AppSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMgtApp
+{
+    public class AppSettings
+    {
+        public const string ConnectionString = "server=localhost;user=root;password=Pa$$word";
+        public const string ConnectionString2 = "server=localhost;user=root;password=Pa$$word;database=FoodMgtApp";
+    }
+}
