c583ad9cc5e8e8bb09443ba829e02b0b29ee6a14
unknown <kawtharkehinde@gmail.com>
2025-12-08 16:10:05 +0100

    asd

--- Context/FoodContext.cs
+++ Context/FoodContext.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace FoodDeliveryApp.Context
 {
     public class FoodContext
     {
-        
+        public const string ConnectionString = "server=localhost;user=root;password=Pa$$word";
+        public const string ConnectionString2 = "server=localhost;user=root;password=Pa$$word;database=FoodMgtApp";
+
+        public MySqlConnection CreateSchema()
+        {
+            using(var connection = new MySqlConnection(ConnectionString2))
+            {
+                connection.Open();
+                return connection;
+            }
+        }
+
+        public MySqlConnection CreateConnectionString()
+        {
+            using(var con = new mysqlconn)
+        }
     }
 }
